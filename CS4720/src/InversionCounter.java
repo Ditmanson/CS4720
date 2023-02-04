67dce10c11b921e921bfa6af84f69a4b1550b0bb
Ditmanson <97914433+Ditmanson@users.noreply.github.com>
2023-02-04 10:12:20 -0700

    inversion count is off

--- CS4720/src/InversionCounter.java
+++ CS4720/src/InversionCounter.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 public class InversionCounter {
     public static void main(String[] args) throws Exception {
         int[] testArray = {54044 ,14108,79294,29649,25260, 60660, 2995, 53777,49689,9083};
+        //int [] testArray = {1,3,2};
         int [] otherTestArray = Arrays.copyOf(testArray, testArray.length);
+        //int [] otherTestArray = {1,3,2};
         int inversions = exhaustiveCount(testArray);
         int inversionsCounted = countInv(otherTestArray);
-        System.out.printf("Inversions counted from exhaustive inversion counter %d\n",inversions);
-        System.out.printf("Inversions counted from mergesort inversion counter %d\n",inversionsCounted);
+        //System.out.printf("Inversions counted from exhaustive inversion counter %d\n",inversions);
+        System.out.printf("Inversions counted from mergesort inversion counter:\t%d\n",inversionsCounted);
+        System.out.println("sorted array:");
         for(int i : otherTestArray){
             System.out.printf("%d   ", i);
         }
@@ -29,7 +32,7 @@ public class InversionCounter {
     public static int countInv(int []array){//input array
         int inversionCount = 0;
         if (array.length==0 || array.length==1){
-            return 0; // not in book pseudocode but it's silly to go through nlogn length algorithm if array is too small for inversions
+            return 0; // base case
         }
         else{
             int middle = array.length/2;
@@ -51,12 +54,17 @@ public class InversionCounter {
         // While there are elements in both the left and right arrays, pick the smaller
         // element and add it to the original array
         while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) { //book psedocode uses b for left and c for right here
-                System.out.printf("No Inversion\nleft: %d\nright: %d\n",left[i],right[j]);
+            if (left[i] <= right[j]) {
+                /*
+                 * book psedocode uses b for left and c for right here, i changed the names for readability
+                 * I also noticed the books psedocode doesn't use a less than or equal to, 
+                 * I think that means he's assumiong each value is going to be different
+                 */
+                System.out.printf("No Inversion\nleft: %d\tright: %d\n",left[i],right[j]);
                 array[k++] = left[i++];
 
             } else {
-                System.out.printf("inversion found\nleft: %d\nright: %d\n",left[i],right[j]);
+                System.out.printf("Inversion\nleft: %d\tright: %d\n",left[i],right[j]);
                 array[k++] = right[j++];
                 inversionCount++;
             }
@@ -65,15 +73,19 @@ public class InversionCounter {
         // Add any remaining elements from the left array
         
         while (i < left.length) {
-            System.out.printf("inversion found\nleft: %d\nright: %d\n",left[i],right[j-1]);
+            System.out.println("Inversion, right sides empty");
+            System.out.printf("new left: %d\t far right: %d\n", left[i], right[j-1] );
             array[k++] = left[i++];
             inversionCount++;
         }
 
         // Add any remaining elements from the right array
         while (j < right.length) {
-            System.out.printf("No Inversion\nleft: %d\nright: %d\n",left[i-1],right[j]);
+            //System.out.printf("No Inversion\nleft: %d\nright: %d\n",left[i-1],right[j]);
+            System.out.println("No Inversion, left empty");
+            System.out.printf("far left: %d\t new right: %d\n", left[i-1], right[j] );
             array[k++] = right[j++];
+            
         }
         return inversionCount;
     }
